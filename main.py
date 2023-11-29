b828baf28ebb68c29a9a6319041bc3d570b5d6e2
Montano <gmontano@kpmg.com>
2023-11-29 14:57:45 -0600

    update main.py

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ import uvicorn
 
 app = FastAPI()
 
+@app.exception_handler(Exception)
+async def error_handler(request, exc):
+    return {"detail": f"An error occurred: {exc}"}
 
 @app.get("/select_all_tables/{table_name}")
 def select_all_tables_(table_name: str):
-    return select_all_tables(table_name)
-
+    try:
+        return select_all_tables(table_name)
+    except Exception as e:
+        raise e  # Propagate the exception to the global error handler
 
 @app.post("/select_one_row")
 def select_one_row_(json: dict):
-    return select_one_row(json)
-
+    try:
+        return select_one_row(json)
+    except Exception as e:
+        raise e  # Propagate the exception to the global error handler
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
